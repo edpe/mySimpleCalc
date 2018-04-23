dd25bd9fd56cc77da84fccfaef9ec18818648b70
Ed Perkins <ed@edperkins.co.uk>
2018-04-23 14:18:06 +0100

    store text value

--- src/components/screens/Calc.js
+++ src/components/screens/Calc.js
@@ -2,12 +2,20 @@ import React, { Component } from "react";
 import { View, Text, TextInput, StyleSheet } from "react-native";
 
 class Calc extends Component{
+
+  handleInput(inputText){
+    console.log(inputText);
+    this.setState({
+      text: inputText
+    })
+  }
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: "green" }}>
       <TextInput
-        style = {styles.input}
-      />>
+        onChangeText={this.handleInput.bind(this)}
+        value={"0"}
+        style = {styles.input}/>
       </View>
     );
   }
